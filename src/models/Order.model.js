706c1b3f31d96b480b676dfa0668a4c9227885e1
Facundo Gimenez <facuie014@gmail.com>
2024-03-24 13:30:14 -0300

    orderModel

---
+++ src/models/Order.model.js
@@ -0,0 +1,22 @@
+import { Schema, model } from 'mongoose'
+const orderSchema = new Schema(
+	{
+		orderDate: Date,
+		product: {
+			type: Schema.Types.ObjectId,
+			ref: 'Products',
+			// required: true,
+		},
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: 'Users',
+			// required: true,
+		},
+	},
+	{
+		timestamps: true,
+        versionKey: false
+	}
+)
+
+export const OrderModel = model('Order', orderSchema)
