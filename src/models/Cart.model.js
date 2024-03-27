af2d75754450c4e0eff05f7ecdd8dd9c5529a937
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-03-26 21:09:53 -0300

    added Cart.model.js

---
+++ src/models/Cart.model.js
@@ -0,0 +1,22 @@
+import { Schema } from 'mongoose'
+
+const cartSchema = new Schema({
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+		required: [true, 'The User is required and must be a valid user'],
+	},
+	orders: {
+		type: [Schema.Types.ObjectId],
+		ref: 'Order',
+	},
+	bookings: {
+		type: [Schema.Types.ObjectId],
+		ref: 'Booking',
+	},
+	total: {
+		type: Number,
+	},
+})
+
+export const CartModel = model('Cart', cartSchema)
