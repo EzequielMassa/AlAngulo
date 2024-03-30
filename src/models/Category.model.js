06d4114e44ac29c661f6c8f0e2427300086e950e
FedericoFrau <fedeafrau@gmail.com>
2024-03-30 00:27:01 -0300

    category crud completed

--- src/models/Category.model.js
+++ src/models/Category.model.js
@@ -4,8 +4,8 @@ const CategorySchema = new Schema({
     name : {
         type : String,
         required : [true, "The name is required"],
-        minLength : [3, "The field description must contain at least 3 characters"],
-        maxLength : [100, 'The field description must be 100 characters maximum'],
+        minLength : [3, "The field name must contain at least 3 characters"],
+        maxLength : [100, 'The field name must be 100 characters maximum'],
         unique : true
     },
     description : {
@@ -14,6 +14,9 @@ const CategorySchema = new Schema({
         minLength : [3, "The field description must contain at least 3 characters"],
         maxLength : [500, 'The field description must be 500 characters maximum']
     }
+},
+{
+    versionKey : false
 });
 
 export const CategoryModel = model("Category", CategorySchema);
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAllCategories, createCategory } from "../controllers/category.controllers.js";
+import { getAllCategories, createCategory, deleteCategory, updateCategory } from "../controllers/category.controllers.js";
 
 const router = Router();
 
 router.get("/categories", getAllCategories);
 router.post("/category", createCategory);
+router.delete("/category/delete/:id", deleteCategory);
+router.put("/category/update/:id", updateCategory);
 
 export default router;
