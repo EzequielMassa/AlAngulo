78e22c3a4d44e7f229056ae8895df03854ec1ee1
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-04-18 23:14:14 -0300

    fixed verifySignUp admin role

--- src/middlewares/verifySignUp.js
+++ src/middlewares/verifySignUp.js
@@ -1,29 +1,26 @@
-import { UserModel } from "../models/User.model.js";
-import {ROLES} from "../models/Role.model.js";
+import { ROLES } from '../models/Role.model.js'
+import { UserModel } from '../models/User.model.js'
 
 export const checkExistingUser = async (req, res, next) => {
-    try {
-     
-      const email = await UserModel.findOne({ email: req.body.email });
-      if (email)
-        return res.status(400).json({ message: "The email already exists" });
-  
-      next();
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
+	try {
+		const email = await UserModel.findOne({ email: req.body.email })
+		if (email)
+			return res.status(400).json({ message: 'The email already exists' })
+
+		next()
+	} catch (error) {
+		res.status(500).json({ message: error.message })
+	}
+}
 
 export const checkExistingRole = (req, res, next) => {
-    if(req.body.roles){
-        for(let i =0;i<req.body.roles.length;i++){
-            if(!ROLES.includes(req.body.roles[i])){
-                return res.status(400).json({
-                    message:`Role ${req.body.roles[i]} does not exist`
-                })
-            }
-        }
-    }
-  
-    next();
-  };
+	if (req.body.role) {
+		if (!ROLES.includes(req.body.role)) {
+			return res.status(400).json({
+				message: `Role ${req.body.role} does not exist`,
+			})
+		}
+	}
+
+	next()
+}
