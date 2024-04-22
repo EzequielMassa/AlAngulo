a0175948383328c76a91ac09a39ad59e387d65ea
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2024-04-22 01:53:36 -0300

    fix cors

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 	res.header(
 		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization,x-access-token'
 	)
 	next()
 })
